630cd5e0fec3bd9a9ecd6fb8578fe5cc91544096
Cameron Ford <c.r.ford@outlook.com>
2022-12-10 21:15:14 -0700

    header is done

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Head from "./Head";
+
+export default function App () {
+    return(
+        <Head />
+    )
+}
---
+++ src/Head.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Head () {
+    return(
+        <div>
+            <img className="headImg" src="https://wikiclipart.com/wp-content/uploads/2017/07/Happy-sun-in-sun-clipart-download.png"/>
+            <p className="headText">Weather Capstone</p>
+        </div>
+    )
+}
