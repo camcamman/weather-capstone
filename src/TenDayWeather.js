92344de8b2b9ff84800ee66e43fad560fd801b1f
Cameron Ford <c.r.ford@outlook.com>
2022-12-12 21:01:54 -0700

    fixed the api call it was not working in last commit. there are icons! but now weather info yet. no css -header and body color

--- src/TenDayWeather.js
+++ src/TenDayWeather.js
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect} from "react";
 import { weatherContext } from "./Context";
-import TenDayWeatherCom from "./TenDayWeatherComponent"
+import TenDayWeatherComponent from "./TenDayWeatherComponent"
 import axios from 'axios';
 
 export default function TenDayWeather () {
@@ -12,22 +12,33 @@ export default function TenDayWeather () {
 
     useEffect(() => {
         //would do 10 but the api only lets me do 3 day for free 
+        // api.get(`http://api.weatherapi.com/v1/forecast.json?key=fd911d07ca5441b7b9014014220912&q=84003&days=10`)
         api.get(`/forecast.json?key=fd911d07ca5441b7b9014014220912&q=84003&days=10`)
         .then(response => {
-            forecastArray.push(response.data.forecast.forecastday)
-
+            // console.log(response.data.forecast.forecastday)
+            // console.log(response.data.forecast)
+            // console.log(response.data)
+            // forecastArray.push(response.data.forecast.forecastday)
+            // forecastArray.push(response.data.forecast)
+            // forecastArray.push(response.data)
+            response.data.forecast.forecastday.map((theItem) => {
+                // forecastArray.push(response.data.forecast.forecastday)
+                // console.log(theItem)
+                forecastArray.push(theItem)
+            })
         })
         .catch((error) => console.error(error))
     }, [])
 
     const killZip = useContext(weatherContext)
-    console.log(forecastArray)
+    // console.log(forecastArray)
 
     killZip.setStateFunctions.killingZipButtonFunction()
     return(
         <div>
-            <TenDayWeatherCom forecastArray = {forecastArray} />
             <h1>ten day weather</h1>
+            <TenDayWeatherComponent 
+            forecastArray = {forecastArray}/>
         </div>
     )
 }
--- src/TenDayWeatherComponent.js
+++ src/TenDayWeatherComponent.js
@@ -1,12 +1,24 @@
 import React from "react";
 
-export default function LongName (props) {
-    const betterProps = props.forecastArray[0]
-    console.log(betterProps)
+export default function TenDayWeatherComponent (props) {
+    // console.log(props)
+    // console.log(props.forecastArray)
 
-    const mappedBetterProps = betterProps.map(() => {
+    const betterProps = props.forecastArray
+
+    // const mappedBetterProps = betterProps.map((theItem) => console.log("worked"))
+    const mappedBetterProps = betterProps.map((theItem) => {
+        // console.log("mapping")
+        // console.log(theItem.day.condition.icon)
         return(
-            <div></div>
+            <div>
+                <img src={theItem.day.condition.icon}/>
+            </div>
         )
     })
+    // console.log(mappedBetterProps)
+    return(
+        <div>{mappedBetterProps}</div>
+        // <div>test</div>
+    )
 }
